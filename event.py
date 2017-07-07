3a1d7b33b044fdbde7d7ff2ab4707c89f8d7862e
cloooze <cloooze@gmail.com>
2017-07-07 16:37:00 +0200

    new file

---
+++ event.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from db_manager import DBManager
+import logging.config
+
+
+class Event(object):
+
+    def __init__(self):
+        self.dbman = DBManager('cpower.db')
+        self.logger = logging.getLogger('cpower')
+
+    def execute(self):
+        raise NotImplementedError()
+
+    def notify(self):
+        raise NotImplementedError()
+
