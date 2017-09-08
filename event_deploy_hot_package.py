555f16b5be8d497e9e31724cb389c38d22bdeab6
cloooze <cloooze@gmail.com>
2017-09-08 14:56:45 +0200

    vlink create in hot deploy event

--- event_deploy_hot_package.py
+++ event_deploy_hot_package.py
@@ -35,18 +35,18 @@ class DeployHotPackage(Event):
         vnf_id = self.order_json['data']['order']['orderItems'][0]['deployHotPackage']['id']
 
         if self.order_status == 'ERR':
+            self.dbman.query('SELECT vn_group_id,vn_left_id,vn_right_id FROM vnf, vn WHERE vnf.vnf_type=? AND vnf.vnf_operation=? AND '
+                             'vnf.vnf_status=? AND vnf.vn_group_id=vn.vn_group_id', (vnf_type, 'CREATE', 'PENDING'))
+            result = self.dbman.fetchone()
+            self.event_params = {'vn_group_id': result['vn_group_id'], 'vn_left_id': result['vn_left_id'], 'vn_right_id': result['vn_right_id']}
+
             # self.dbman.query('UPDATE vnf SET vnf_status=? WHERE vnf_type=? AND vnf_operation=? AND vnf_status=?', ('ERROR', vnf_type, 'CREATE', 'PENDING'), False)
             self.dbman.query('DELETE FROM vnf WHERE vnf_type=? AND vnf_operation=? AND vnf_status=?', (vnf_type, 'CREATE', 'PENDING'))
 
-            self.dbman.query('SELECT vn_group_id FROM vnf WHERE vnf_type=? AND vnf_operation=? AND vnf_status=?', (vnf_type, 'CREATE', 'ERROR'))
-            result = self.dbman.fetchone()
-            self.event_params = {'vn_group_id': result['vn_group_id']}
-
             return 'FAILURE'
 
-
         # Updating just created VNF
-        self.dbman.query('UPDATE vnf SET vnf_id=?, vnf_status=? WHERE vnf_type=? AND vnf_operation=? AND vnf_status=?', (vnf_id, 'COMPLETE',  vnf_type, 'CREATE', 'PENDING'), False)
+        self.dbman.query('UPDATE vnf SET vnf_id=?, vnf_status=? WHERE vnf_type=? AND vnf_operation=? AND vnf_status=?', (vnf_id, 'COMPLETE',  vnf_type, 'CREATE', 'PENDING'))
 
         # Getting VM details
         r = ecm_util.invoke_ecm_api(vnf_id + '?$expand=vms', c.ecm_service_api_vapps, 'GET')
@@ -56,20 +56,20 @@ class DeployHotPackage(Event):
         vm_name = resp['data']['vapp']['vms'][0]['name']
 
         # Getting VMVNIC details
-        r = ecm_util.invoke_ecm_api(vm_id + '$expand=vmvnics', c.ecm_service_api_vms, 'GET')
+        r = ecm_util.invoke_ecm_api(vm_id + '?$expand=vmvnics', c.ecm_service_api_vms, 'GET')
         resp = json.loads(r.text)
 
         vmvnics_list = list()
 
         for vmvnic in resp['data']['vm']['vmVnics']:
-            vmvnic_id = resp['data']['vm']['vmVnics'][0]['id']
-            vmvnic_name = resp['data']['vm']['vmVnics'][0]['name']
+            vmvnic_id = vmvnic['id']
+            vmvnic_name = vmvnic['name']
             vmvnic_ip_address = vmvnic['internalIpAddress'][0]
             vmvnic_vim_object_id = vmvnic['vimObjectId']
-            vmvnics_list.append(tuple(vmvnic_id, vm_id, vmvnic_name, vmvnic_ip_address, vmvnic_vim_object_id))
+            vmvnics_list.append((vmvnic_id, vm_id, vmvnic_name, vmvnic_ip_address, vmvnic_vim_object_id))
 
         # Saving everything into DB
-        self.dbman.save_vm(tuple(vm_id, vnf_id, vm_name), False)
+        self.dbman.save_vm((vm_id, vnf_id, vm_name), False)
         self.dbman.save_vmvnic(vmvnics_list[0], False)
         self.dbman.save_vmvnic(vmvnics_list[1], False)
         self.dbman.save_vmvnic(vmvnics_list[2], False)
@@ -78,21 +78,13 @@ class DeployHotPackage(Event):
 
         self.logger.info('All information about VM and VMVNICs successfully stored into DB.')
 
-
-
-        # create VLINK
-        # TODO
-
-
-
-
         # Checking if a second hot deploy is required
         self.dbman.query('SELECT vnf_type FROM network_service,vnf WHERE network_service.customer_id=? AND '
                          'network_service.ntw_service_id=vnf.ntw_service_id AND vnf.vnf_operation=? AND vnf.vnf_status=?', (customer_id, 'CREATE', 'PENDING'))
         result = self.dbman.fetchone()
 
         if result is not None:
-            # DEPLOY HOT PACKAGE HERE
+            # DEPLOY SECOND HOT PACKAGE HERE
             vnf_type = result['vnf_type']
             hot_package_id = 'dd1f60a6-f735-412c-a10a-4f282a891ca2'  # TODO Put it in config
             hot_file_json = load_json_file('./json/deploy_hot_package.json')
@@ -109,20 +101,120 @@ class DeployHotPackage(Event):
             self.logger.info('Depolying HOT package %s' % hot_package_id)
             ecm_util.deploy_hot_package(hot_package_id, hot_file_json)
 
+
+        # CREATE VLINK
+        # I need:
+        # service_id
+        # vnf type list
+        # vn name left
+        # vn name right
+        # vn vimobject id left
+        # vn vimobject id right
+
+        # Getting service_id
+        self.dbman.query('SELECT ntw_service_id FROM vnf WHERE vnf_id=?', (vnf_id))
+        result = self.dbman.fetchone()
+        service_id = result['ntw_service_id']
+
+        # Getting vnf type list
+        self.dbman.query('SELECT vnf_type FROM vnf WHERE ntw_service_id=?', (service_id))
+        result = self.dbman.fetchall()
+        vnf_type_list = (r['vnf_type'] for r in result)
+        self.logger.info('DEBUG ---- VNF TYPE LIST generata %s' % vnf_type_list)
+
+        # Building vn names
+        vn_name_l = customer_id + '-' + 'left'
+        vn_name_r = customer_id + '-' + 'right'
+
+        # Getting vn vimobject IDs
+        self.dbman.query('SELECT vn_left_vimobject_id,vn_right_vimobject_id FROM vn_group,vnf WHERE vnf.vnf_id=? AND vnf.vn_group_id = vn_group.vn_group_id')
+        result = self.dbman.fetchone()
+        vn_vimobject_id_l = result['vn_left_vimobject_id']
+        vn_vimobject_id_r = result['vn_right_vimobject_id']
+
+        # Getting route targets
+        self.dbman.query('SELECT rt_left,rt_right,rt_mgmt FROM network_service WHERE ntw_service_id=?', (service_id))
+        result = self.dbman.fetchone()
+        rt_left = result['rt_left']
+        rt_right = result['rt_right']
+        rt_mgmt = result['rt_mgmt']
+
+        self.logger.info('Creating VLINK object...')
+
+        vlink_json = load_json_file('json/create_vlink.json')
+        vlink_json['orderItems'][0]['createVLink']['name'] = customer_id + '-SDN-policy'
+        vlink_json['orderItems'][0]['createVLink']['service']['id'] = service_id
+
+        ex_input = load_json_file('json/extensions_input_create.json')
+
+        # In case of multiple VNF, duplicating the entire service-instance tag
+        policy_rule_list = list()
+
+        for vnf_type_el in vnf_type_list:
+            vm_vnic_name_left = customer_id + '-' + vnf_type_el + '-left'
+            self.dbman.query('SELECT vm_vnic_vimobject_id FROM vmvnic WHERE vm_vnic_name=?', (vm_vnic_name_left,))
+            vm_vnic_vimobject_id_left = self.dbman.fetchone()['vm_vnic_vimobject_id']
+
+            vm_vnic_name_right = customer_id + '-' + vnf_type_el + '-right'
+            self.dbman.query('SELECT vm_vnic_vimobject_id FROM vmvnic WHERE vm_vnic_name=?', (vm_vnic_name_right,))
+            vm_vnic_vimobject_id_right = self.dbman.fetchone()['vm_vnic_vimobject_id']
+
+            service_instance = {
+                'operation': 'create',
+                'si_name': customer_id + '-' + vnf_type_el,
+                'left_virtual_network_fqdn': 'default-domain:cpower:' + vn_name_l,
+                'right_virtual_network_fqdn': 'default-domain:cpower:' + vn_name_r,
+                'service_template': 'cpower-template',
+                'port-tuple': {
+                    'name': 'porttuple-' + customer_id + '-' + vnf_type_el,
+                    'si-name': customer_id + '-' + vnf_type_el
+                },
+                'update-vmvnic': {
+                    'left': vm_vnic_vimobject_id_left,
+                    'right': vm_vnic_vimobject_id_right
+                }
+            }
+
+            ex_input['extensions-input']['service-instance'].append(service_instance)
+
+            policy_rule_list.append('default-domain:cpower:' + customer_id + '-' + vnf_type_el)
+
+        ex_input['extensions-input']['network-policy']['policy_name'] = customer_id + '_policy'
+        ex_input['extensions-input']['network-policy']['src_address'] = 'default-domain:cpower:' + vn_name_l
+        ex_input['extensions-input']['network-policy']['dst_address'] = 'default-domain:cpower:' + vn_name_r
+
+        ex_input['extensions-input']['update-vn-RT']['right_VN'] = vn_vimobject_id_r
+        ex_input['extensions-input']['update-vn-RT']['right_RT'] = rt_right
+        ex_input['extensions-input']['update-vn-RT']['left_VN'] = vn_vimobject_id_l
+        ex_input['extensions-input']['update-vn-RT']['left_RT'] = rt_left
+        ex_input['extensions-input']['update-vn-RT'][
+            'network_policy'] = 'default-domain:cpower:' + customer_id + '_policy'
+
+        ex_input['extensions-input']['network-policy']['policy-rule'] = policy_rule_list
+
+        vlink_json['orderItems'][0]['createVLink']['customInputParams'][0]['value'] = json.dumps(ex_input)
+
+        ecm_util.invoke_ecm_api(None, c.ecm_service_api_orders, 'POST', vlink_json)
+
     def rollback(self):
-        self.dbman.rollback() # to rollback manual insert into db in case of error
+        # if self.order_status == 'ERR' or self.event_params['rollback'] == 'YES':
+        if self.order_status == 'ERR':
+            self.dbman.rollback() # to rollback manual insert into db in case of error
 
-        self.logger.info('Rollbacking VNs creation...')
+            self.logger.info('Rollbacking VNs creation...')
 
-        vn_group_id = self.event_params['vn_gruop_id']
+            vn_group_id = self.event_params['vn_group_id']
+            vn_left_id = self.event_params['vn_left_id']
+            vn_right_id = self.event_params['vn_right_id']
 
-        ecm_util.invoke_ecm_api(vn_group_id, c.ecm_service_api_vns, 'DELETE')
+            ecm_util.invoke_ecm_api(vn_left_id, c.ecm_service_api_vns, 'DELETE') # what if it goes wrong?? :(
+            ecm_util.invoke_ecm_api(vn_right_id, c.ecm_service_api_vns, 'DELETE')
 
-        self.dbman.delete_vn_group(vn_group_id)
+            self.dbman.delete_vn_group(vn_group_id)
 
 
-        # TODO
-        # manually delete VN and NETWORK SERVICE(?)
+            # TODO
+            # manually delete VN and NETWORK SERVICE(?)
 
 
 
